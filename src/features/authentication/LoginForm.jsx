b3e77bb8c78285e37f8f0669e232a738fbf93037
rishikesh <121371895+rixshhh@users.noreply.github.com>
2024-02-15 00:47:26 +0530

    Update LoginForm.jsx

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("rishikesh@gmail.com");
-  const [password, setPassword] = useState("1234567890");
+  const [email, setEmail] = useState(" ");
+  const [password, setPassword] = useState(" ");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
